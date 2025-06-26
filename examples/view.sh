2738f4ede96de793930928c6b1295f1d0915f95a
Rickyeeeeee <rickyhsu2017@gmail.com>
2025-06-26 18:41:09 +0000

    Add view script for textured Gaussian models
    This commit introduces a new script, view.sh, which facilitates the visualization of textured Gaussian models. The script sets a scene variable and calls the textured_gaussians_viewer.py with the appropriate checkpoint paths for different model outputs. This addition aims to streamline the process of viewing and analyzing model results.

---
+++ examples/view.sh
@@ -0,0 +1,5 @@
+scene="24"
+python textured_gaussians_viewer.py --ckpt \
+    /workspace/work/Outputs/dtu/textured_gaussians_a_pc10k_t16/scan${scene}/ckpts/ckpt_29999.pt \
+    /workspace/work/Outputs/dtu/textured_gaussians_rgb_pc10k_t16/scan${scene}/ckpts/ckpt_29999.pt \
+    /workspace/work/Outputs/dtu/textured_gaussians_rgba_pc10k_t16/scan${scene}/ckpts/ckpt_29999.pt 
